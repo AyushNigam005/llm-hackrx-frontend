90c84a2a869c92e5d7a8e1a9bf262fc255eae444
AyushNigam005 <142025542+AyushNigam005@users.noreply.github.com>
2025-07-31 23:17:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if st.button("Get Answers 🚀"):
             }
 
             try:
-                res = requests.post("res = requests.post("https://ayush018-hackrx-fastapi.hf.space/api/v1/hackrx/run", json=payload", json=payload)
+                res = requests.post("https://ayush018-hackrx-fastapi.hf.space/api/v1/hackrx/run", json=payload)
                 if res.status_code == 200:
                     results = res.json()["answers"]
                     st.success("✅ Answers retrieved!")
